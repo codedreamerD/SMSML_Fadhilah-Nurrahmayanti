8d88f3fe291ca22fbc710585f68dd3a078c9c6bc
Fadhilah Nurrahmayanti <adnindyakhadijah@gmail.com>
2025-06-15 18:29:14 +0700

    SMSML_Fadhilah-Nurrahmayanti

--- monitoring_dan_logging/inference.py
+++ monitoring_dan_logging/inference.py
@@ -254,4 +254,4 @@ def predict(features: PerformanceLevel):
             raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
